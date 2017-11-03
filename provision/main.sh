8ac5e84dcf431c658c6a4c14ecea7ddba79ac11d
svoboond <svoboond@gmail.com>
2017-11-03 08:42:11 +0100

    Remove creating Django superuser and loading data

--- provision/main.sh
+++ provision/main.sh
@@ -20,11 +20,6 @@ sudo apt-get install -y python-gdal;
 
 python manage.py migrate;
 
-# create user 'admin'
-echo "from django.contrib.auth.models import User; User.objects.filter(email='admin@example.com').delete(); User.objects.create_superuser('admin', 'admin@example.com', 'nimda')" | python manage.py shell
-python manage.py loaddata geonode/base/fixtures/initial_data.json;
-python manage.py loaddata geonode/base/fixtures/default_oauth_apps.json;
-
 # install Java 8
 cd /tmp;
 wget --quiet --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-x64.tar.gz -O jdk.tar.gz;
