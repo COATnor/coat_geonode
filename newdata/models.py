17e7e89ad4ed7537770bdba3fc5d473f4f2fc637
mdlux <matteo.destefano@nina.no>
2018-01-25 15:43:13 +0100

    creating draft of Moth model

--- newdata/models.py
+++ newdata/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Moth(models.Model):
+    date = models.DateTimeField()
+    alt = models.IntegerField()
+    location = models.CharField(max_length=12)
+    waypoint = models.CharField(max_length=12)
+    station = models.PositiveSmallIntegerField()
+    ep = models.PositiveIntegerField()
+    opl = models.PositiveIntegerField()
+    opint = models.PositiveIntegerField()
+    opdark = models.PositiveIntegerField()
+    opsum = models.PositiveIntegerField()
+    agr = models.PositiveIntegerField()
+    obs = models.CharField(max_length=12)
+    branches = models.PositiveSmallIntegerField()
+    notes = models.CharField()
+
